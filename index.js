fcacd09bb41e7ff3c09071c383c38511fb035ea5
voinuecaterina´@gmail.com <voinuecaterina@gmail.com>
2021-09-13 16:14:42 +0200

    Fix error

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const chalk = require('chalk');
 const randomColor = require('randomcolor');
 const prompt = require('prompt-sync')();
-
-const color = randomColor({
-  luminosity: process.argv[2],
-  hue: process.argv[3],
-});
+let luminosity = process.argv[2];
+let hue = process.argv[3];
 
 if (process.argv[2] === 'ask') {
   luminosity = prompt('Specify the luminosity:');
   hue = prompt('specify the hue:');
 }
+
+const color = randomColor({
+  luminosity: luminosity,
+  hue: hue,
+});
 
 console.log(
   chalk.hex(color)(`###############################
