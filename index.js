fb4b1e861ccee01cbdec135285a291866b913dc6
voinuecaterina´@gmail.com <voinuecaterina@gmail.com>
2021-09-20 12:14:20 +0200

    Add change places of luminosity and hue

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const chalk = require('chalk');
 const randomColor = require('randomcolor');
 const prompt = require('prompt-sync')();
-let luminosity = process.argv[2];
-let hue = process.argv[3];
+let hue = process.argv[2];
+let luminosity = process.argv[3];
 
 if (process.argv[2] === 'ask') {
-  luminosity = prompt('Specify the luminosity:');
   hue = prompt('specify the hue:');
+  luminosity = prompt('specify the brightness:');
 }
 
 const color = randomColor({
-  luminosity: luminosity,
   hue: hue,
+  luminosity: luminosity,
 });
 
 console.log(
